c3c83cef1cb4c9b9dd08141f8d1a14e66750c9bd
nub69 <shabik.ashab@gmail.com>
2022-08-16 13:08:07 +0600

    basic of stack added

--- stack.cpp
+++ stack.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+    // Architecture of stack -> last in first out (LIFO)
+    // we can create stack with array and linked list
+    // operation of stack
+    // Top() -> last element is top
+    // push -> to insert something in the stack
+    // push algorithom
+    // s1: top++;
+    // s2: insert the value on top
+    // pop -> delete last operation in stack
+    // pop algorithom
+    // s1: delete top value
+    // s2: top--;
+
+    // stack using array
+
+    
     
     return 0;
 }
